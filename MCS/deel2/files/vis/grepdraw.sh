5a321088d377b990074855ba16ee1eb3e8682ed4
jessab <jessa.bekker@gmail.com>
2013-12-04 12:16:55 +0100

    clean

--- MCS/deel2/files/vis/grepdraw.sh
+++
@@ -1,8 +0,0 @@
-sed -i 's/[.]/.\n/g' $1
-cat $1 | grep "idpd"  | $2 -I &
-
-sleep 0.5s
-
-# wmctrl -a MyTerminal
-# wmctrl -r idpdraw -e 0,0,0,700,900
-## IF YOU have wmcrtrl installed, use the above to set your cursor at the correct position
